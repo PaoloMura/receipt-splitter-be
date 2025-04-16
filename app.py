31de1a78006ce5c511330873ace92df7e5f39192
FaieqR <faieqrahman@gmail.com>
2025-04-16 15:29:49 +0100

    Implement AI API to the app.py

--- app.py
+++ app.py
@@ -3,8 +3,25 @@ import base64
 import io 
 from PIL import Image 
 import pytesseract
+from google import genai
+from dotenv import load_dotenv
+import os
+from PIL import Image
+import json
+import re
 
 app = Flask(__name__)
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+# Access the API key from environment variables
+api_key = os.getenv('API_KEY')
+
+client = genai.Client(api_key=api_key)
+
+prompt = "Extract the list of items and put it into a json with the following format: /{name:, quantity, price:,}."
+
 
 @app.route('/upload-receipt', methods=['POST'])
 def upload_receipt():
@@ -23,11 +40,36 @@ def upload_receipt():
         image_bytes = base64.b64decode(image_b64)
         image = Image.open(io.BytesIO(image_bytes))
 
-        extracted_text = pytesseract.image_to_string(image)
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=[image, prompt]
+        )       
+
+        raw_text = response.text.strip()
+
+        # Remove markdown-style ```json ... ```
+        if raw_text.startswith("```json"):
+            raw_text = re.sub(r"^```json\s*|\s*```$", "", raw_text.strip())
+
+        # Parse the JSON string into a Python object
+        try:
+            parsed_json = json.loads(raw_text)
+        except json.JSONDecodeError as e:
+            print("JSON parsing failed:", e)
+            with open("raw_failed_output.txt", "w") as f:
+                f.write(raw_text)
+            exit(1)
+
+        # Print the parsed_json as a JSON string
+        json_data = json.dumps(parsed_json, indent=2, ensure_ascii=False)
+        print(json_data)
+        print(type(json_data))
+
+        # extracted_text = pytesseract.image_to_string(image)
 
         return jsonify({
             "message": "Image received successfully!",
-            "raw_text": extracted_text}), 200
+            "raw_text": json_data}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
