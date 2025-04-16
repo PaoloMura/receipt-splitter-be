ee842b25fbee3aca24e6ce61b1e0d401dfc772c0
FaieqR <faieqrahman@gmail.com>
2025-04-16 14:04:14 +0100

    Sending image and prompt to Geminit 2.0

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 from google import genai
 from dotenv import load_dotenv
+import os
+from PIL import Image
 
 # Load the environment variables from the .env file
 load_dotenv()
@@ -10,10 +12,19 @@ api_key = os.getenv('API_KEY')
 # Use the API key in your application
 print(f'Your API key is: {api_key}')
 
-client = genai.Client(api_key="GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
 
+# response = client.models.generate_content(
+#     model="gemini-2.0-flash",
+#     contents=["How does AI work?"]
+# )
+# print(response.text)
+
+prompt = "Extract the list of items and put it into a json with the following format: /{name:, quantity, price:,}."
+
+image = Image.open("sampleImage2.jpg")
 response = client.models.generate_content(
     model="gemini-2.0-flash",
-    contents=["How does AI work?"]
+    contents=[image, prompt]
 )
 print(response.text)
